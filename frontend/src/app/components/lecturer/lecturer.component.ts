4ab616be797f32e4c7900d6f5b5e383cb0a1d9d4
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2025-09-12 22:32:22 +0200

    FRONTEND: Admin, Lecturer's page

---
+++ frontend/src/app/components/lecturer/lecturer.component.ts
@@ -0,0 +1,12 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-lecturer',
+  imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './lecturer.component.html',
+  styleUrl: './lecturer.component.css'
+})
+export class LecturerComponent {
+
+}
--- frontend/src/app/pages/dashboards/admin/lecturers/lecturers.component.ts
+++ frontend/src/app/pages/dashboards/admin/lecturers/lecturers.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LecturerComponent } from "../../../../components/lecturer/lecturer.component";
 
 @Component({
   selector: 'app-lecturers',
-  imports: [],
+  imports: [RouterLink, LecturerComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './lecturers.component.html',
   styleUrl: './lecturers.component.css'
 })
